9c7da2f01bfad682e6bb3980650bdf9466cc7b69
Suphawit Hngosakul - 6609650053 <145352874+Suphawit-Hngosakul@users.noreply.github.com>
2024-12-03 01:45:46 +0700

    basic code

---
+++ Backend/src/test/java/ac/th/tu/cs/Backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.th.tu.cs.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
